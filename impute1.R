c488989f5813c8fbc5b6ad6e8b791bb193ae354c
carlosmanpipe <84484424+carlosmanpipe@users.noreply.github.com>
2025-01-22 19:19:59 -0500

    Update impute1.R

--- impute1.R
+++ impute1.R
@@ -1,1 +1,7 @@
-# Include here your code for your first chosen imputation method
+db$maxEducLevel<- factor(db$maxEducLevel)
+
+dummy_maxEducLevel <- as.data.frame(model.matrix(~ maxEducLevel - 1, data = db)) 
+#db<- db  %>% cbind(dummy_maxEducLevel) 
+db <- cbind(db, dummy_maxEducLevel)
+
+db %>% select(maxEducLevel,maxEducLevel1, maxEducLevel2,maxEducLevel3 ,maxEducLevel4 ,maxEducLevel5 ,maxEducLevel6, maxEducLevel7 ) %>% head() # Include here your code for your first chosen imputation method
